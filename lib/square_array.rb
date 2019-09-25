35481dd9a99a5416a4143248a5d3fd816b571071
Sydney Werbach <sydwer@gmail.com>
2019-09-25 05:10:13 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,6 +1,7 @@
 def square_array(array)
   counter = 0
   while counter < array.length do
-    array[counter]**2 
+    new_array = array<< array[counter]**2 
     counter += 1
+    return new_array
 end
